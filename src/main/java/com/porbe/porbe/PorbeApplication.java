3659e664d52d4b38df5bd5c929eed55f09369835
Alejandro Meneses <almenesesp@gmail.com>
2025-04-07 00:36:07 -0500

    initial config

---
+++ src/main/java/com/porbe/porbe/PorbeApplication.java
@@ -0,0 +1,13 @@
+package com.porbe.porbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porbe/porbe/PorbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porbe.porbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
